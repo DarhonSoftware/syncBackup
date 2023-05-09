7646afd514d49480b0d69666e1f146e24f02040e
Daniel Mendizabal <daniel@darhon.com>
2023-05-09 22:12:30 +1000

    Corrections with translation

---
+++ syncbackup_en.ts
@@ -0,0 +1,473 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="qml/AboutDialog.qml" line="10"/>
+        <location/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CAbout</name>
+    <message>
+        <location filename="about.cpp" line="18"/>
+        <source>Backup and mirror your drives</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="20"/>
+        <source>It offers the most common options getting most of the power from rsync.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="21"/>
+        <source>It takes advantage of the delta-transfer algorithm, which reduces the amount of data sent over the network by sending only the differences between the source files and the existing files in the destination.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="25"/>
+        <source>It finds files that need to be transferred using a &apos;quick check&apos; algorithm that looks for files that have changed in size or in last-modified time.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="28"/>
+        <source>Copyright</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="29"/>
+        <source>Help &amp; Support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="31"/>
+        <source>Privacy Policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CBackend</name>
+    <message>
+        <location filename="backend.cpp" line="142"/>
+        <source>You have one of the following problems with the source/destination:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="143"/>
+        <source>The source or destination path is empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="144"/>
+        <source>One of the directories does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="145"/>
+        <source>Source and destination are ssh remote.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="185"/>
+        <source>-no-config-file-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="361"/>
+        <source>ERROR [%1] REPORTED BY THE BACKEND DURING EXCECUTION</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="374"/>
+        <source>BACKEND HAS CRASHED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="backend.cpp" line="379"/>
+        <source>ERROR [%1] REPORTED BY THE BACKEND AT EXIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CommandlineDialog</name>
+    <message>
+        <location filename="qml/CommandlineDialog.qml" line="11"/>
+        <location/>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/CommandlineDialog.qml" line="46"/>
+        <location/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExcIncHelpDialog</name>
+    <message>
+        <location filename="qml/ExcIncHelpDialog.qml" line="8"/>
+        <location/>
+        <source>Guidelines to exclude or include files and directories</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpDialog</name>
+    <message>
+        <location filename="qml/HelpDialog.qml" line="8"/>
+        <location/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SSHDialog</name>
+    <message>
+        <location filename="qml/SSHDialog.qml" line="12"/>
+        <location/>
+        <source>Address to remote server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/SSHDialog.qml" line="47"/>
+        <location/>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/SSHDialog.qml" line="63"/>
+        <location/>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/SSHDialog.qml" line="68"/>
+        <location/>
+        <source>USER@HOST:PATH (username@200.150.150.11:backup_folder)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>initpage</name>
+    <message>
+        <location filename="qml/initpage.qml" line="17"/>
+        <location/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="44"/>
+        <location/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="52"/>
+        <location/>
+        <source>Save As</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="60"/>
+        <location/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="94"/>
+        <location filename="qml/initpage.qml" line="140"/>
+        <location/>
+        <source>Local</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="102"/>
+        <location filename="qml/initpage.qml" line="148"/>
+        <location/>
+        <source>Remote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="116"/>
+        <location/>
+        <source>SOURCE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="162"/>
+        <location/>
+        <source>DESTINATION</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="196"/>
+        <location/>
+        <source>Run</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="206"/>
+        <location/>
+        <source>Run the process as a trial without making changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="218"/>
+        <location/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="232"/>
+        <location/>
+        <source>WARNINGS in current configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="241"/>
+        <location/>
+        <source>Delete files in the destination, if they are no longer in the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="246"/>
+        <location/>
+        <source>Delete files in the destination, if they are marked as excluded in the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="251"/>
+        <location/>
+        <source>Replace files in the destination, even if modified time is newer than the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="278"/>
+        <location/>
+        <source>Open configuration file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="293"/>
+        <location/>
+        <source>The configuration file couldn&apos;t be opened, it may not be in the correct format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="303"/>
+        <location/>
+        <source>Save configuration file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="318"/>
+        <location/>
+        <source>The configuration file couldn&apos;t be saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="328"/>
+        <location/>
+        <source>Select folder for SOURCE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="340"/>
+        <location/>
+        <source>Select folder for DESTINATION</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/initpage.qml" line="376"/>
+        <location/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mainwindow</name>
+    <message>
+        <location filename="qml/mainwindow.qml" line="12"/>
+        <location/>
+        <source>&amp;Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/mainwindow.qml" line="14"/>
+        <location/>
+        <source>&amp;Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/mainwindow.qml" line="22"/>
+        <location/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/mainwindow.qml" line="27"/>
+        <location/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/mainwindow.qml" line="33"/>
+        <location/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/mainwindow.qml" line="56"/>
+        <location/>
+        <source>Alert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/mainwindow.qml" line="58"/>
+        <location/>
+        <source>The process is running, do you still want to close the application?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <location filename="qml/settings.qml" line="14"/>
+        <location/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="43"/>
+        <location/>
+        <source>Configure files in destination</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="49"/>
+        <location/>
+        <source>Set the &lt;u&gt;group&lt;/u&gt; of the destination file to be the same as the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="55"/>
+        <location/>
+        <source>Set the &lt;u&gt;owner&lt;/u&gt; of the destination file to be the same as the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="61"/>
+        <location/>
+        <source>Set the &lt;u&gt;permission&lt;/u&gt; of the destination file to be the same as the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="67"/>
+        <location/>
+        <source>When a &lt;u&gt;symlink&lt;/u&gt; is encountered, recreates the symlink in the destination</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="79"/>
+        <location/>
+        <source>Configure actions in destination</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="85"/>
+        <location/>
+        <source>Delete files in the destination, if they are no longer in the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="91"/>
+        <location/>
+        <source>Delete files in the destination, if they are marked as excluded in the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="97"/>
+        <location/>
+        <source>Only update existing files in the destination. Do not transfer new files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="103"/>
+        <location/>
+        <source>Do not replace files in the destination, if they already exist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="109"/>
+        <location/>
+        <source>Do not replace files in the destination, when modified time is newer than the source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="122"/>
+        <location/>
+        <source>Configure actions in source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="128"/>
+        <location/>
+        <source>Copy directories recursively</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="135"/>
+        <location/>
+        <source>Avoid transferring files larger than</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="166"/>
+        <location/>
+        <source>List of files and directories to be excluded and included</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="182"/>
+        <location/>
+        <source>Compose PATH to insert in Excluded or Included</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="185"/>
+        <location/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="201"/>
+        <location/>
+        <source>EXCLUDED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="207"/>
+        <location filename="qml/settings.qml" line="258"/>
+        <location/>
+        <source>Insert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="218"/>
+        <location filename="qml/settings.qml" line="269"/>
+        <location/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/settings.qml" line="252"/>
+        <location/>
+        <source>INCLUDED</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
