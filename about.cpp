6b5a29070295af3ac7b6b6ba556ee42b1c90b9eb
Daniel Mendizabal <daniel@darhon.com>
2026-06-14 13:52:34 +1000

    Release 2.1.5
    - Metadata update

--- about.cpp
+++ about.cpp
@@ -23,7 +23,7 @@ void CAbout::init()
                    + tr("It finds files that need to be transferred using a 'quick check' algorithm "
                         "that looks for files that have changed in size or in last-modified time."));
     setIconDarhon("qrc:/darhon/images/darhon.png");
-    setCopyright(tr("Copyright") + " 2010-2024 - Darhon Software");
+    setCopyright(tr("Copyright") + " 2010-2026 - Darhon Software");
     setSupport(tr("Help & Support") + " - " + QCoreApplication::organizationDomain());
     setPrivacy(QString("<a href='https://darhon.com/syncbackup-privacy-policy'>%1</a>").arg(tr("Privacy Policy")));
     setVersion("");
